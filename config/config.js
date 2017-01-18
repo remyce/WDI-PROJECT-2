5481dae69e2e037c7eb73864c4ffde7f870ef123
remyce <remycewilsonhughes@gmail.com>
2017-01-18 10:09:51 +0000

    Adding process.env

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/wdi-project-2',
-  secret: process.env.SECRET || 'this is a secret.'
+  db: process.env.MONGODB_URI || 'mongodb://localhost/wdi-project-2',
+  secret: process.env.SECRET || 'riot-app-secret'
 };
